a2bfc4e0b975849271011fcceabe8fa6357acbcb
Roescoe <52107557+Roescoe@users.noreply.github.com>
2025-05-07 07:08:56 -0700

    Add table for resources

--- eliteDangerousColonisation.py
+++ eliteDangerousColonisation.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import ast
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import *
 
@@ -38,47 +39,78 @@ class MainWindow(QDialog):
             lineEdits.append(lineEdit)
         self.dialogLayout.addWidget(QLabel("Logfile folder (usually under Saved Games):"))
         folderLoad = lineEdits[0]
-        with open("allColonyLandings.txt", "r") as f:
-            testFileLine = f.readline()
-            if testFileLine.startswith("Folder_location:"):
-                folderLoad.setText(testFileLine.split("Folder_location: ",1)[1].strip())
-                print("found default folder:", testFileLine.split("Folder_location: ",1)[1].strip())
+        if os.path.exists("settings.txt"):
+            with open("settings.txt", "r") as f:
+                testFileLine = f.readline()
+                if testFileLine.startswith("Folder_location:"):
+                    folderLoad.setText(testFileLine.split("Folder_location: ",1)[1].strip())
+                    print("found default folder:", testFileLine.split("Folder_location: ",1)[1].strip())
         self.dialogLayout.addWidget(folderLoad, 0, 1)
         
         loadFolderButton = QPushButton()
         loadFolderButton.setText("Load Folder")
         
         self.dialogLayout.addWidget(loadFolderButton, 0, 2)
-
+        self.selectProjectButton = QPushButton()
+        self.projectDropdown = QComboBox()
         
         quitButton = QPushButton()
         quitButton.setText("Quit")
 
-        self.dialogLayout.addWidget(quitButton, 2, 0)
+        self.dialogLayout.addWidget(quitButton, 100, 0)
         self.setLayout(self.dialogLayout)
         
         loadFolderButton.clicked.connect(lambda: loadFile(self, lineEdits[0].text()))
+        self.selectProjectButton.clicked.connect(lambda: populateTable(self))
         quitButton.clicked.connect(lambda: quitNow())
 
 def loadFile(self, directory):
     print("loading file")
     logFileList = setUpLogfile(directory)
     data = findUniqueEntries(["ColonisationConstructionDepot"], "MarketID")
+    with open("settings.txt", "w") as s:
+        s.write("Folder_location: ")
+        s.write(directory)
     with open("allColonyLandings.txt", "w") as f:
-        f.write("Folder_location: ")
-        f.write(directory)
-        f.write("\n")
-        f.write(str(data))
-        f.write("\n")
-        f.write(str(uniqueStations))
-        f.write("\n")
-    formatDropdown = QComboBox()
+        f.write("\n".join(map(str, data)))
+
+    
     print("values: ",uniqueStations.values())
-    formatDropdown.addItems(uniqueStations.values())
-    self.dialogLayout.addWidget(formatDropdown, 1, 1)
-    SelectProjectButton = QPushButton()
-    SelectProjectButton.setText("Select Project")
-    self.dialogLayout.addWidget(SelectProjectButton, 1, 2)
+    self.projectDropdown.addItems(uniqueStations.values())
+    self.dialogLayout.addWidget(self.projectDropdown, 1, 1)
+    self.selectProjectButton.setText("Select Project")
+    self.dialogLayout.addWidget(self.selectProjectButton, 1, 2)
+    self.setLayout(self.dialogLayout)
+
+def populateTable(self):
+    print("Populating table:")
+    startIndex = 5
+    projectID = -1
+
+    currentSelectedProjectName = self.projectDropdown.currentText()
+    
+    projectID = [key for key, val in uniqueStations.items() if val == currentSelectedProjectName]
+    print("project ID: ", projectID)
+    print("project name: ", self.projectDropdown.currentText())
+    self.dialogLayout.addWidget(QLabel("Resource"), 4, 0)
+    self.dialogLayout.addWidget(QLabel("Total Need"), 4, 1)
+    self.dialogLayout.addWidget(QLabel("Current Need"), 4, 2)
+    with open("allColonyLandings.txt", "r") as f:
+        for line in f:
+            testFileLine = ast.literal_eval(line)
+            if testFileLine["MarketID"] == projectID[0]:
+                resources = testFileLine["ResourcesRequired"]
+                print("resources: ",resources)
+                for i in range(len(resources)):
+                    print("from file:", resources[i])
+                    print("type? ", type(resources))
+                    print("resource: ", resources[i]["Name_Localised"])
+                    print("resource: ", resources[i]["RequiredAmount"])
+                    self.dialogLayout.addWidget(QLabel(resources[i]["Name_Localised"]), startIndex, 0)
+                    self.dialogLayout.addWidget(QLabel(str(resources[i]["RequiredAmount"])), startIndex, 1)
+                    self.dialogLayout.addWidget(QLabel(str(resources[i]["RequiredAmount"]-resources[i]["ProvidedAmount"])), startIndex, 2)
+                    startIndex += 1
+    print("total resources: ", startIndex-3)
     self.setLayout(self.dialogLayout)
 
 def quitNow():
