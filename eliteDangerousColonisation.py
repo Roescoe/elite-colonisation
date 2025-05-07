d995a6aaa4831adc99108774375c50333e3366ac
Roescoe <52107557+Roescoe@users.noreply.github.com>
2025-05-06 22:29:53 -0700

    Add load functionality

--- eliteDangerousColonisation.py
+++ eliteDangerousColonisation.py
@@ -6,7 +6,9 @@ from PyQt6.QtWidgets import *
 
 
 logFileList = []
-data = []
+
+uniqueStations = {}
+
 
 def setUpLogfile(directory):
     folderdir = directory
@@ -18,79 +20,102 @@ def setUpLogfile(directory):
                 logFileList.append(os.path.join(path, name))
     return logFileList.sort()
 
-def setUpWindow():
-    print("setting up window")
-    app = QApplication([])
-    window = QWidget()
-    window.setWindowTitle("Roescoe's Elite Colonisation App")
-    window.setGeometry(100, 100, 1000, 100)
-    dialogLayout = QGridLayout()
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        print("setting up window")
+        
+        self.setWindowTitle("Roescoe's Elite Colonisation App")
+        self.setGeometry(100, 100, 1000, 100)
+        self.dialogLayout = QGridLayout()
 
 
-    lineEdits = []
-    for x in range(5):
-        lineEdit = QLineEdit()
-        lineEdit.setMaxLength(300)
-        lineEdits.append(lineEdit)
-    dialogLayout.addWidget(QLabel("Logfile folder (usually under Saved Games):"), 0, 0)
-    dialogLayout.addWidget(lineEdits[0], 0, 1)
-    
-    loadFolderButton = QPushButton()
-    loadFolderButton.setText("Load Folder")
+        lineEdits = []
+        for x in range(5):
+            lineEdit = QLineEdit()
+            lineEdit.setMaxLength(300)
+            lineEdits.append(lineEdit)
+        self.dialogLayout.addWidget(QLabel("Logfile folder (usually under Saved Games):"))
+        folderLoad = lineEdits[0]
+        with open("allColonyLandings.txt", "r") as f:
+            testFileLine = f.readline()
+            if testFileLine.startswith("Folder_location:"):
+                folderLoad.setText(testFileLine.split("Folder_location: ",1)[1].strip())
+                print("found default folder:", testFileLine.split("Folder_location: ",1)[1].strip())
+        self.dialogLayout.addWidget(folderLoad, 0, 1)
+        
+        loadFolderButton = QPushButton()
+        loadFolderButton.setText("Load Folder")
+        
+        self.dialogLayout.addWidget(loadFolderButton, 0, 2)
+
+        
+        quitButton = QPushButton()
+        quitButton.setText("Quit")
+
+        self.dialogLayout.addWidget(quitButton, 2, 0)
+        self.setLayout(self.dialogLayout)
+        
+        loadFolderButton.clicked.connect(lambda: loadFile(self, lineEdits[0].text()))
+        quitButton.clicked.connect(lambda: quitNow())
+
+def loadFile(self, directory):
+    print("loading file")
+    logFileList = setUpLogfile(directory)
+    data = findUniqueEntries(["ColonisationConstructionDepot"], "MarketID")
+    with open("allColonyLandings.txt", "w") as f:
+        f.write("Folder_location: ")
+        f.write(directory)
+        f.write("\n")
+        f.write(str(data))
+        f.write("\n")
+        f.write(str(uniqueStations))
+        f.write("\n")
     formatDropdown = QComboBox()
-    dialogLayout.addWidget(loadFolderButton, 0, 3)
-    formatDropdown.addItem("Audio - MP3")
-    formatDropdown.addItem("Video - MP4")
-    dialogLayout.addWidget(formatDropdown)
-    quitButton = QPushButton()
-    quitButton.setText("Quit")
-
-    dialogLayout.addWidget(quitButton)
-    window.setLayout(dialogLayout)
-    window.show()
-    loadFolderButton.clicked.connect(lambda: loadFile(lineEdits[0].text()))
-    quitButton.clicked.connect(lambda: quitNow())
-    sys.exit(app.exec())
-    
+    print("values: ",uniqueStations.values())
+    formatDropdown.addItems(uniqueStations.values())
+    self.dialogLayout.addWidget(formatDropdown, 1, 1)
+    SelectProjectButton = QPushButton()
+    SelectProjectButton.setText("Select Project")
+    self.dialogLayout.addWidget(SelectProjectButton, 1, 2)
+    self.setLayout(self.dialogLayout)
 
 def quitNow():
     sys.exit()
 
-def loadFile(directory):
-    print("loading file")
-    logFileList = setUpLogfile(directory)
-    findUniqueEntries(["ColonisationConstructionDepot"], "MarketID")
-    with open("allColonyLandings.txt", "w") as f:
-        f.write(str(data))
-
 
 def findUniqueEntries (eventList, uniqueId):
+    data = []
     uniqueIDs = []
     for logfile in logFileList:
         with open(logfile) as f:
             for line in f:
+                rawLine = json.loads(line)
+                if "MarketID" in rawLine and "StationName" in rawLine: 
+                    uniqueStations[rawLine["MarketID"]] = rawLine["StationName"]
                 if any(event in line for event in eventList):
-                    rawLine = json.loads(line)
                     if(rawLine.get(uniqueId) not in uniqueIDs):
                         data.append(rawLine)
                         uniqueIDs.append(rawLine.get(uniqueId))
                         print(rawLine.get(uniqueId))
+
+    for key in list(uniqueStations.keys()):
+        if key not in uniqueIDs:
+            del uniqueStations[key]
+
+
+    print("ids: ", uniqueIDs)
+    print("Stations: ", uniqueStations.keys())
+
     return data
 
-# def printEntries (data, eventList, eventAttributeList):
-#     if len(eventAttributeList) is not len(eventList):
-#         return
-#     for e in data:
-#         for selectedEvent, item in zip(eventList, eventAttributeList):
-#             if e["event"] == selectedEvent:
-#                 with open("output3.txt", "a") as f:
-#                     f.write("%s: " % selectedEvent)
-#                     f.write("\n    ")
-#                     for i in item:
-#                         f.write(str(e[i]))
-#                     f.write("")
-
-setUpWindow()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+# setUpWindow()
 
 
 
